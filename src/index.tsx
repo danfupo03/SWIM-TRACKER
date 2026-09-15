4e8225f814d85d6717ba4a7dbc98d9dc457838ff
danfupo03 <danfupo@outlook.com>
2026-09-14 20:27:47 -0600

    feat(Routing): Replace Home component with DashboardPage and add HistoryPage route

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,27 @@ import { html, Html } from "@elysiajs/html";
 import { staticPlugin } from "@elysiajs/static";
 import { timeController } from "./modules/time/time.controller";
 import { timeService } from "./modules/time/time.service";
-import { Home } from "./views/home";
+import { statsController } from "./modules/stats/stats.controller";
+import { statsService } from "./modules/stats/stats.service";
+import { DashboardPage } from "./views/dashboard";
+import { HistoryPage } from "./views/history";
 import "./db/database";
 
 const app = new Elysia()
   .use(html())
   .use(staticPlugin({ assets: "public", prefix: "/public" }))
   .use(timeController)
+  .use(statsController)
   .get("/", () => (
-    <Home
+    <DashboardPage
+      summary={statsService.getSummary()}
+      personalBests={statsService.getPersonalBests()}
+      tests={timeService.findTests()}
+      selectedTest={statsService.findMostSwumTest()}
+    />
+  ))
+  .get("/history", () => (
+    <HistoryPage
       times={timeService.findAll()}
       seasons={timeService.findSeasons()}
       tests={timeService.findTests()}

3e4a847349dc6121f33159930ee41348be8fdecf
danfupo03 <danfupo@outlook.com>
2026-09-14 19:02:31 -0600

    feat(Layout): Add data-theme attribute and stylesheet link to layout component

--- src/views/layout.tsx
+++ src/views/layout.tsx
@@ -1,11 +1,12 @@
 import { Html, type PropsWithChildren } from "@elysiajs/html";
 
 export const Layout = ({ children }: PropsWithChildren) => (
-  <html lang="en">
+  <html lang="en" data-theme="cupcake">
     <head>
       <meta charset="utf-8" />
       <title>Swim Tracker</title>
       <script src="https://unpkg.com/htmx.org@2.0.3"></script>
+      <link rel="stylesheet" href="/public/output.css" />
     </head>
     <body>{children}</body>
   </html>
